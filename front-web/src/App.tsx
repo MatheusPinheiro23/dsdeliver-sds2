7346795770adff5e866f4da6d83142893dac20c7
Matheus Pinheiro <matheus.pinheiro1996@hotmail.com>
2021-01-10 15:05:11 -0300

    Search order location

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Navbar from './Navbar';
+
 import Routes from './Routes';
 
 
@@ -7,6 +7,7 @@ function App() {
   return (
     
      <Routes />
+   
     
   );
 }
--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -2,26 +2,30 @@
 
 
 function StepsHeader() {
-    return (
-        <header className="orders-steps-container">
-            <div className="orders-steps-content">
-                <h1 className="steps-title" >
-                    SIGA AS <br />ETAPAS
-                </h1>
-                <ul className="steps-items">
-                    <li>
-                        <span className="steps-number">1</span>
-                  Selecione os produtos e localização
-                    </li>
+  return (
+    <div className="orders-container">
+     < header className="orders-steps-container">
+         <div className="orders-steps-content">
+             <h1 className="steps-title"> 
+             SIGA AS<br/> ETAPAS
+             </h1>
 
-                    <li>
-                        <span className="steps-number">1</span>
-Depois clique em<strong>"ENVIAR PEDIDO"</strong>
-    </li>
-                </ul>
-            </div>
-        </header>
-    )
+             <ul className="steps-items">
+                 <li>
+                     <span className="steps-number">1</span>
+                     Selecione os produtos e localização
+                 </li>
+                 <li>
+                 <span className="steps-number">2</span>
+                     Depois clique em <strong>"ENVIAR PEDIDO"</strong>
+                 </li>
+             </ul>
+         </div>
+         </ header>
+     
+
+    </div>
+  )
 }
 
 export default StepsHeader;
